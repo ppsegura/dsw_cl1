0de77c6b0a6091b0851fb7ff6fb1f9f2552cb08a
pzpenas <pedro.pe.segura@gmail.com>
2022-04-27 12:00:11 -0500

    version final CL1

--- src/main/java/app/APP_PSEGURA_REGISTRO.java
+++ src/main/java/app/APP_PSEGURA_REGISTRO.java
@@ -13,6 +13,7 @@ import model.Carrera;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -21,9 +22,17 @@ import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class APP_PSEGURA_REGISTRO extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	private JPanel contentPane;
 	
@@ -80,6 +89,17 @@ public class APP_PSEGURA_REGISTRO extends JFrame {
 		scrollPane.setViewportView(txtSalida);
 		
 		txtCodigo = new JTextField();
+		txtCodigo.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char M = e.getKeyChar();
+				
+				if(M<'0' || M>'9') {
+					e.consume();
+					JOptionPane.showMessageDialog(null, "Ingrese solo números");
+				}
+			}
+		});
 		txtCodigo.setBounds(104, 11, 86, 20);
 		contentPane.add(txtCodigo);
 		txtCodigo.setColumns(10);
@@ -101,6 +121,16 @@ public class APP_PSEGURA_REGISTRO extends JFrame {
 		contentPane.add(lblNombre);
 		
 		txtNombre = new JTextField();
+		txtNombre.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if(txtNombre.getText().length() >= 50)
+			    {
+			        e.consume();
+			        JOptionPane.showMessageDialog(null, "Solo puede ingresar 50 carácteres");
+			    }
+			}
+		});
 		txtNombre.setColumns(10);
 		txtNombre.setBounds(104, 39, 144, 20);
 		contentPane.add(txtNombre);
@@ -110,6 +140,16 @@ public class APP_PSEGURA_REGISTRO extends JFrame {
 		contentPane.add(lblSexo);
 		
 		txtSexo = new JTextField();
+		txtSexo.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if(txtSexo.getText().length() >= 50)
+			    {
+			        e.consume();
+			        JOptionPane.showMessageDialog(null, "Solo puede ingresar 50 carácteres");
+			    }
+			}
+		});
 		txtSexo.setColumns(10);
 		txtSexo.setBounds(104, 83, 144, 20);
 		contentPane.add(txtSexo);
@@ -119,6 +159,17 @@ public class APP_PSEGURA_REGISTRO extends JFrame {
 		contentPane.add(lblEdad);
 		
 		txtEdad = new JTextField();
+		txtEdad.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char M = e.getKeyChar();
+				
+				if(M<'0' || M>'9') {
+					e.consume();
+					JOptionPane.showMessageDialog(null, "Ingrese solo números");
+				}
+			}
+		});
 		txtEdad.setColumns(10);
 		txtEdad.setBounds(439, 83, 77, 20);
 		contentPane.add(txtEdad);
@@ -128,6 +179,16 @@ public class APP_PSEGURA_REGISTRO extends JFrame {
 		contentPane.add(lblApellidos);
 		
 		txtApellido = new JTextField();
+		txtApellido.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if(txtApellido.getText().length() >= 50)
+			    {
+			        e.consume();
+			        JOptionPane.showMessageDialog(null, "Solo puede ingresar 50 carácteres");
+			    }			    	
+			}
+		});
 		txtApellido.setColumns(10);
 		txtApellido.setBounds(438, 37, 213, 20);
 		contentPane.add(txtApellido);
@@ -137,19 +198,100 @@ public class APP_PSEGURA_REGISTRO extends JFrame {
 		contentPane.add(lblPeso);
 		
 		txtPeso = new JTextField();
+		txtPeso.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char M = e.getKeyChar();
+				
+				if((M<'0' || M>'9') && (M == '.' )){
+					e.consume();
+					JOptionPane.showMessageDialog(null, "Ingrese solo números");
+				}
+					
+			}
+		});
 		txtPeso.setColumns(10);
 		txtPeso.setBounds(104, 135, 77, 20);
 		contentPane.add(txtPeso);
 		
 		llenaCombo();
 	}
+	
 
 	void llenaCombo() {
+		
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql");
+
+		EntityManager em = fabrica.createEntityManager();
+
+		// -- combo categorias
+
+		TypedQuery<Carrera> consulta = 
+
+				em.createQuery("select u from Carrera u", Carrera.class);
+
+		List<Carrera> lstCarrera = consulta.getResultList();
+
+		cboCarrera.addItem("Seleccionar...");
+		for(Carrera c : lstCarrera){
+			cboCarrera.addItem(c.getIdescripcion());
+		}
+		
+		em.close();
 		
 	}
 	
 	void registrar() {
 		
+		// entradas
+
+				int idcodigo = Integer.parseInt(txtCodigo.getText());
+
+				String nombre = txtNombre.getText();
+				
+				String apellido = txtApellido.getText();
+				
+				String sexo = txtSexo.getText();
+
+				int edad = Integer.parseInt(txtEdad.getText());
+				
+				double peso = Double.parseDouble(txtPeso.getText());
+
+				int carrera = cboCarrera.getSelectedIndex();
+
+
+				Alumnos p = new Alumnos();
+
+				p.setIdcod(idcodigo);
+
+				p.setInombre(nombre);
+
+				p.setIapellido(apellido);
+
+				p.setIsexo(sexo);
+				
+				p.setIedad(edad);
+
+				p.setIpeso(peso);
+
+				p.setIdcar(carrera);
+
+				
+
+				EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql");
+
+				EntityManager em = fabrica.createEntityManager();
+
+				em.getTransaction().begin();
+
+				em.persist(p);
+
+				em.getTransaction().commit();
+
+
+				em.close();
+
+				txtSalida.setText("Alumno registrado..!");
 
 	}
 }
